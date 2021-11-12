7872b36c67dd0b8466c9922946c815c1020dcb8a
Hyury Luis da Silva Noll <hyury@weg.net>
2021-11-12 09:40:58 -0300

    second commit

--- main.py
+++ main.py
@@ -26,23 +26,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.post("/files/")
-async def create_file(file: bytes = File(...)):
-
-
-    print(file)
-    return {"file_size": len(file)}
-
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...)):
